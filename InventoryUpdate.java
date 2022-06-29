f8e647c0955a2875a502ddb2d31f24703c83c50b
Agustín Matsubara <66968800+aematsubara@users.noreply.github.com>
2022-06-29 15:26:57 -0300

    Fixed some issues.

--- InventoryUpdate.java
+++ InventoryUpdate.java
@@ -22,6 +22,7 @@
 package me.matsubara.roulette.util;
 
 import com.cryptomorin.xseries.ReflectionUtils;
+import com.google.common.base.Preconditions;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
@@ -108,7 +109,7 @@ public final class InventoryUpdate {
      */
 
     public static void updateInventory(JavaPlugin plugin, Player player, String newTitle) {
-        Lang3Utils.notNull(player, "Cannot update inventory to null player.");
+        Preconditions.checkArgument(player != null, "Cannot update inventory to null player.");
 
         try {
             // Get EntityPlayer from CraftPlayer.
@@ -190,10 +191,19 @@ public final class InventoryUpdate {
 
         if (extraNames != null && extraNames.length > 0) {
             if (extraNames.length == 1) return getField(refc, instc, extraNames[0]);
-            return getField(refc, instc, extraNames[0], Lang3Utils.remove(extraNames, 0));
+            return getField(refc, instc, extraNames[0], removeFirst(extraNames));
         }
 
         return null;
+    }
+
+    private static String[] removeFirst(String[] array) {
+        int length = array.length;
+
+        String[] result = new String[length - 1];
+        System.arraycopy(array, 1, result, 0, length - 1);
+
+        return result;
     }
 
     private static MethodHandle getFieldHandle(Class<?> refc, Class<?> inscofc, String name) {
